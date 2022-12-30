096cee0e9812b88627c9752fc284705e672bb289
Anand Jaiswal <ajstyles0524@gmail.com>
2022-12-30 15:39:30 +0530

    added code for median of two sorted array

--- src/main/scala/dataStructure/Arrays/Median.scala
+++ src/main/scala/dataStructure/Arrays/Median.scala
@@ -1,7 +1,7 @@
 package dataStructure.Arrays
 
 object Median extends App{
-  private def median(array1: Array[Int], array2: Array[Int]): Double = {
+  private def median1(array1: Array[Int], array2: Array[Int]): Double = {
     val size1 = array1.size
     val size2 = array2.size
     val combined = new Array[Int](size1 + size2)
@@ -40,7 +40,18 @@ object Median extends App{
     }
   }
 
+  private def median2(array1: Array[Int], array2: Array[Int]): Double = {
+    val combined = (array1 ++ array2).sorted
+    val size = combined.size
+    if (size % 2 == 0) {
+      (combined(size / 2 - 1) + combined(size / 2)) / 2.0
+    } else {
+      combined(size / 2).toDouble
+    }
+  }
+
   private val array1 = Array(1, 3, 5, 7)
   private val array2 = Array(2, 4, 6)
-  println(median(array1, array2))
+  println(median1(array1, array2))
+  println(median2(array1, array2))
 }
